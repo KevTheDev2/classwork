7dd7063b025d5f960a883e02afe8fad79f4cbdd7
Kevin Gibbs II <kgibbs136@gmail.com>
2025-03-27 21:58:33 -0400

    first commit

---
+++ Unit-2-Intro-To-C#/First-ProjectA/Program.cs
@@ -0,0 +1,60 @@
+﻿// This is a comment - everything after it is ignored
+
+/*
+ *  This is a block comment. EVerything between the slash-asterisk 
+ *  and asterisk-slash is ignored
+ *  
+ *  Useful if you have a lot to say or if you want to deactivate some code
+ *  
+ the * at the start of each line is optional
+ * */
+
+// a namespace identifies a context in which a word is known
+// Allows the same name to be used in different context to mean different things
+// Indents in the code are for humans to read easier
+namespace First_ProjectA
+{
+    // everything in C# is defined in a class
+    // class - a group of related things*
+    // a Class starts with the class keyword
+    class Program
+
+        // { } surround blocks of code aka code-blocks or scope/identifiyer
+    { // Start of the code or class
+            // Every application has a method called Main()
+            // a method is a self-contained code that performs a function
+            // aka function or program
+            // A method may return a value and accept data to process
+            // Method signature identifies what the method returns, it's name, and what it accepts
+            // Return-type name(data-it-accepts)
+            // Main() is where every application starts
+            // Every applications has one and only one Main() method
+
+            // Below main() is a method that returns void and accepts string[] called args
+            //
+            // void means it retuens nothing
+            // static means there can only be one of these in this block (program class)
+           
+        
+            // return
+            // type  name(data-it-accepts)
+        static void Main(string[] args) // method signature
+        {// Start of the code for Main()
+            // This code will display whatever is between then("") on the screen
+
+            // Console is an object defined by C# to represent the screen
+            // an object has certain behaviors
+            // A behavior is something an object can do with data
+            // Behavior are implemented using methods
+
+            // object.method(data) - object oriented programming
+            //
+            // when you see a "." after a name, the name to the left is probably an object
+            //          name( , the name is a method)
+            //          name after ".", the name is usually a method 
+            //      object.method(data-for-the-method-to-process)
+
+            Console.WriteLine("Hello, KGII!"); // EVERY C# STATEMENT ENDS WITH A ";"
+        }
+    } // End of the code for main()
+} // end of the code for the class
---
+++ Unit-2-Intro-To-C#/FirstProgram/Program.cs
@@ -0,0 +1,125 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace FirstProgram
+{
+
+    // This program will ask for three numbers
+    //      add them together and display the total
+
+    // **Programming is finding the solutuon first. Coding is building/converting it with a programming language**
+    // Thinking like a human first, then a coder second
+    // Computers don't understand human context. You have to be specific details
+    // Programming Solution - Human thinking dictates the following:
+
+    // Ask the numbers one at a time
+    // Write down each number as it is given.
+    // Add the number 
+    // Tell the requester the total (display)
+
+    // Now that we have thee steps in our solution...
+    // Identify any data/processes needed for the steps
+
+    // 1. A place to write down each number - a variable in a program (something that needs to be remembered)
+    // 2. A place to hold the sum/total - a variable in a program
+    // 3. A way to ask for the numbers - a method in a program (a way to do something) (object.method())
+    // 4. A way to receive the numbers - a method in a program (object.method())
+    // 5. A way to report the total/sum - a method in a program (object.method())
+    //      * In object oriented prgramming, we'll need an object and method
+
+    // Place the steps in the order and provide more detail, if needed, to solve the problem.
+    // 1. Ask the numbers one at a time
+    //      a. Tell whatever is giving us a number to give me the first number
+    //      b. Recieve the first number from sender
+    //      c. Write down the first number
+    //      d. Tell whatever is giving us a number to give me the second number
+    //      e. Recieve the second number from sender
+    //      f. Write down the second number
+    //      g. Tell whatever is giving us a number to give me the third number
+    //      h. Recieve the third number from sender
+    //      i. Write down the third number
+
+    // 2. Add the numbers together create a sum
+    //      a. Add the first number to a sum total
+    //      d. Add the second number to a sum total
+    //      c. Add the third number to a sum total
+
+    // 3. Tell the requester the sum/total (display)
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /**************************************************************************
+             *Define any data the program needs at the top of the program
+             *
+             *Data may be defined anywhere before it's used, is easier to find 
+             *at the top of the program
+             *
+             * To define a variable: data-type-name = initial-value:
+             * 
+             * Commonly used data types in C#
+             * 
+             *  int - Numeric data that is a whole number between +/- 2 billion
+             *  long - A whole number greater than +/- 2 billion
+             *  double - numeric value with decimal places
+             *  char _ a single alhpa-numeric character
+             *  string - a series of alpha-numeric characters (words or a sentence)
+             *  bool(ean) - true or false
+             *  
+             *      examples:
+             *      10 - int
+             *      1.0 - double
+             *      'a' - char (note it is enclosed in ' ')
+             *      "some words" - string (note it is enclosed in " ")
+             *      
+             *      variable names in C# are usually spelled in  camelCase; Use - to separate words (optional)
+             *      (CamelCase means the first letter is in lower case wile other 
+             *              words start with an uppercased letter)
+             *              
+             *              It's always good to initialize variables when defining them so you know
+             *              what is in them
+             **************************************************************************
+             *
+             * Define variables to hold the numbers we need to add
+             
+             */
+
+            int number1 = 0;
+            int number2 = 0;
+            int number3 = 0;
+
+            // Define a variable to hold the sum of the numbers
+            
+            int sum = 0;
+
+
+            Console.WriteLine("---Starting Program---");
+
+            // Ask for numbers one at a time using C# console logic
+            //      which represents they keyboard and screen
+            // Console.Readline() returns a string - cannot store as an int
+            //  
+            Console.WriteLine("Please enter a number: "); // Asking for the number
+            string aLine;   // String to hold the line of input from the keyboard
+            aLine = Console.ReadLine();  // Get a line from the keyboard
+            // We need an int value to store our numbers
+            // Console.ReadLine() only returns a string
+            // So we need to convert the string from Console.ReadLine to an int
+            // int.Parse(string) will convert a string to an int
+            number1 = int.Parse(aLine);
+            
+            //**********************************************
+            // Verify that I got the data I expected
+            // Dsiplay some words and the value I receive
+            // "string" + something is called concatenation (it sticks them together)
+            //Console.WriteLine("You entered: " + number1);
+            
+            //
+            
+            //*********************************************
+            Console.WriteLine("---Ending Program---");
+        }
+    }
+}
