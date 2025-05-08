6973bb06ac30b72cd4c909665e18a8fe8f31b4fd
Kevin Gibbs II <kgibbs136@gmail.com>
2025-05-08 18:27:22 -0400

    5.8

---
+++ Unit-4-Intro-To-Object-Oriented-Programming/Day-5-Inheritance-Example-V1/Day-5-Inheritance-Example/ItalianPlayingCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5_Inheritance_Example
+{
+    public class ItalianPlayingCard : PlayingCard
+    {
+        // Define a default ctor that calls the bass clas contructor
+        public ItalianPlayingCard() : base(0, "Joker", "Black"); {}
+
+        // Define a 2-arg ctor for value and suit
+        public ItalianPlayingCard(int theValue, string theSuit, string theColor); 
+    : base(theValue, theSuit, theColor)
+    }
+}
