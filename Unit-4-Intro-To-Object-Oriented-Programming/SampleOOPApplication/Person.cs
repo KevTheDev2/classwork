378ee0acf0c8969ef1e531c93cf7ef5f3fc2ac01
Kevin Gibbs II <kgibbs136@gmail.com>
2025-05-12 18:31:15 -0400

    5.12

---
+++ Unit-4-Intro-To-Object-Oriented-Programming/SampleOOPApplication/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleOOPApplication
+{
+
+    /*This class with represent a person
+     * 
+     * A Class has data (Attributes) and methods behavior()
+     * 
+     * Attributes:  Name 
+     *              Height
+     *              Weight
+     *              Age
+     *              Gender
+     *              EyeColor
+     *              HairColor
+     * 
+     ***************************************/
+    public class Person
+    {
+        /***************
+         * Instance variables indentity attributes unique to an object of a class
+         **************/
+
+        private string _name; // C# coding convention is private variables start with _
+        private double _heightInInches;
+        private double _weightInPounds;
+        private double _age;
+        private string _gender;
+        private string _eyeColor;
+        private string _hairColor;
+
+        /**********************************************************
+         * Properties to allow access to the data
+         **********************************************************/
+
+
+
+    }
+}
