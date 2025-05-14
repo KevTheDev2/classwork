adfa81894bc88debdab8670aaa2d687551d84f9b
Kevin Gibbs II <kgibbs136@gmail.com>
2025-05-14 18:33:56 -0400

    5.14

--- Unit-4-Intro-To-Object-Oriented-Programming/SampleOOPApplication-V1/SampleOOPApplication/Program.cs
+++ Unit-4-Intro-To-Object-Oriented-Programming/SampleOOPApplication-V1/SampleOOPApplication/Program.cs
@@ -8,8 +8,24 @@ class Program
         Console.WriteLine("------ Start of Sample Object Oriented Application Made From Scratch -------");
         
         Person aPerson = new Person("John", 78, 250.6, 69, "Male");
-        Console.WriteLine(aPerson);
-        
-        Console.WriteLine("------ End of Sample Object Oriented Application Made From Scratch -------");
+        Console.WriteLine($"aPerson: {aPerson}");
+
+        Person aPerson2 = new Person("John", 78, 250.6, 69, "Male");
+        Console.WriteLine($"aperson2: {aPerson2}");
+
+        // Compare aPerson and aPerson2 to see if they are equal
+       // if (aPerson == aPerson2) // == compares the reference value (loacation in memory)
+            if (aPerson.Equals(aPerson2)) // .Equals() SHOULD ompare the content of the object
+        {
+            Console.WriteLine("They are equal");
+        }
+        else
+        {
+            Console.WriteLine("They are not equal");
+        }
+
+
+
+            Console.WriteLine("------ End of Sample Object Oriented Application Made From Scratch -------");
     } // End of Main()
 }
