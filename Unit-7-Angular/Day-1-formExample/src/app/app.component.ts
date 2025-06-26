7e1ed1845cea957e9751c2cc6fb289c5f7ddab90
Kevin Gibbs II <kgibbs136@gmail.com>
2025-06-26 09:11:34 -0400

    6.26

--- Unit-7-Angular/Day-1-formExample/src/app/app.component.ts
+++ Unit-7-Angular/Day-1-formExample/src/app/app.component.ts
@@ -5,14 +5,15 @@ import { FormExampleComponent } from './components/form-example/form-example.com
 // @Component identifies all the Angular stuff you are using
 //            including new components
 @Component({
-  selector: 'app-root',
+  selector:      'app-root',                           // Is the name of the tag used in html to reference the component
   standalone: true,
   // imports identifies external resources, like components, used in the app
-  imports: [RouterOutlet, FormExampleComponent],  // Note the name has Component
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, FormExampleComponent],      // Note the name has Component
+  templateUrl: './app.component.html',                // The File containing the html for the component
+  styleUrl:    './app.component.css'                  // the file containing the css for the component
 })
-// Allows processes outside this component to access things defined in the component 
+// Allows processes outside this component to access things defined in the component
+// Any data or methods you want Angular to use must be exported from Typescript file for the component 
 // To use One-Way Binding for data defined in this code in the HTML:  {{variable-name}}
 export class AppComponent {
   pageHeading = 'Welcome to AHBC - C# - Contact Info Submission'
--- Unit-7-Angular/Day-1-formExample/src/app/components/form-example/form-example.component.ts
+++ Unit-7-Angular/Day-1-formExample/src/app/components/form-example/form-example.component.ts
@@ -11,9 +11,23 @@ import { throwError } from 'rxjs';
 })
 // Allow access to stuff defined this component from outside the component
 export class FormExampleComponent {
+  // data to be shared with the Angular when processing the web page
   buttonClicked = false
+  firstName= "" // hold the data from the first name field on the form
+  lastName= "" // hold the data from the first last field on the form
+  birthday= "" // hold the data from the birthday field on the form
+  foodPreference= "" // hold the data from the food preference field on the form
+  emailAddress= "" // hold the data from the email address field on the form
+  sendEmail= "" // hold the data from the send name field on the form
+  sports= "" // hold the data from the sports field on the form
+  // Methods to handle the interactions with html/web page
   onSubmit() { // Handle the Submit button click on the  form
     this.buttonClicked = true
     console.log(`OK.... You Clicked Submit`)
+    console.log(`You entered ${this.firstName} in the First Name field` )
+    console.log(`You entered ${this.lastName} in the Last Name field` )
+    console.log(`You entered ${this.foodPreference} in the food preference field` )
+    console.log(`You entered ${this.emailAddress} in the email field` )
+    console.log(`You entered ${this.sendEmail} in the send field` )
   }
 }
