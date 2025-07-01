82b5e7dcbd002a341494a3dae19026e4b670d308
Kevin Gibbs II <kgibbs136@gmail.com>
2025-06-30 22:01:04 -0400

    6.30.2

--- Unit-7-Angular/Day-4-Multi-Page-App/src/app/app.config.ts
+++ Unit-7-Angular/Day-4-Multi-Page-App/src/app/app.config.ts
@@ -1,8 +1,19 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+// Tell the router where to find the array with the routes
+//    that connects a URL to a component
+// the import name must match the export name in app.rotes.ts 
+import { routes } from './app.routes'; // connect app to app.routes.ts (.ts is assumed)
 
+// appConfig object identifies things that you are using that are not part of 
+//            base Angular processing
+// (the router is not required for basic Angular processing)
+// the providers attribute is a list of optional Angular processes you are using
+// providerRouter indicates you want Angular to router support using the import
+//    specified above
+
+//
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
 };
--- Unit-7-Angular/Day-4-Multi-Page-App/src/app/app.routes.ts
+++ Unit-7-Angular/Day-4-Multi-Page-App/src/app/app.routes.ts
@@ -1,7 +1,23 @@
-import { Routes }        from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { Routes }        from '@angular/router'; // Give me access to Angular 
+import { HomeComponent } from './components/home/home.component'; // Give me access to the home component
+import { SampleFormComponent } from './components/sample-form/sample-form.component';
+import { StateSealsComponent } from './components/state-seals/state-seals.component';
+import { StudentListComponent} from './components/student-list/student-list.component'
+// The Routed Object is an array of objects to associate a URL path to a component
 
+// Each entry in the array contains the following attributes:
+
+//      path - the URL path to e associated witha component
+//      component - name of the import component associated with the path
+//      redirect - optional attribute to redirect the path to a different page
+//      pathMacth - indicates if a full match toa URL path is necesarry to use the entry
+
+// we are defining an object called routes with a data type of Routes
+// We tell the router the name of the array  in app.config.ts
 export const routes: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full' },
-    {path: 'home', component: HomeComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full' }, // empty path should redirect to /home page
+    {path: 'home', component: HomeComponent},            // /home should display the HomeComponent
+    {path: 'whatever', component:SampleFormComponent},    // /whatever should display the SampleFormComponent  
+    {path: 'seals', component: StateSealsComponent},
+    {path: 'student-list', component: StudentListComponent} 
 ];
