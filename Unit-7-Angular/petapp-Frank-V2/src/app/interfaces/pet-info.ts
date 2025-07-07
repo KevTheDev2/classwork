0c0808caa4806c19e035d63989f87765387fa409
Kevin Gibbs II <kgibbs136@gmail.com>
2025-07-07 18:36:05 -0400

    7.7

---
+++ Unit-7-Angular/petapp-Frank-V2/src/app/interfaces/pet-info.ts
@@ -0,0 +1,8 @@
+// interfaces describes the properties of an object
+// Uses so any features needing the data that has the same description
+
+export interface PetInfo {
+    name : string,
+    type : string,
+    gender : string
+}
--- Unit-7-Angular/petapp-Frank-V2/src/app/services/pet.service.ts
+++ Unit-7-Angular/petapp-Frank-V2/src/app/services/pet.service.ts
@@ -1,12 +1,33 @@
+// Services manage data for the app and access to the data
+
 // Created via ng g service services/pet.service
 // Which makes it Dependency Injectable automatically
 //       and creates an empty constructor
 import { Injectable } from '@angular/core';
+import { PetInfo } from '../interfaces/pet-info'
 
 @Injectable({
   providedIn: 'root'
 })
+// the class of the service (aka module) contains data and functions to manage the data
 export class PetService {
 
-  constructor() { }
+   // typically data is kept in an array inside the service
+   // and is described by an interface
+   petData : PetInfo[] = [] // hold data for the service - initially empty
+  constructor() 
+  {
+    this.petData.push({name: 'Pina', type: 'Dog', gender: 'Female'})
+    this.petData.push({name: 'Bella', type: 'Cat', gender: 'Female'})
+    this.petData.push({name: 'Beans', type: 'Dog', gender: 'Male'})
+    this.petData.push({name: 'Zoey', type: 'Cat', gender: 'Female'})
+
+    
+   }
+
+   // provide a function to return the array of data
+   getPetData() : PetInfo[] {
+return this.petData;
+
+   }
 }
