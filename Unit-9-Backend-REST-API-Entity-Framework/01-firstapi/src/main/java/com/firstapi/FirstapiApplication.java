30451b5b901851f6d5a80f95699cf6a72ecd6d98
Kevin Gibbs II <kgibbs136@gmail.com>
2025-07-30 18:34:15 -0400

    7.30

--- Unit-9-Backend-REST-API-Entity-Framework/01-firstapi/src/main/java/com/firstapi/FirstapiApplication.java
+++ Unit-9-Backend-REST-API-Entity-Framework/01-firstapi/src/main/java/com/firstapi/FirstapiApplication.java
@@ -1,13 +1,22 @@
 package com.firstapi;
+// this is generated for you when you create Spring Boot application.
+//			DO NOT CHANGE ANYTHING IN HERE!!!!
 
+// Spring Boot
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This is the main class for the Spring Boot application
+// It contains the main method which is the entry point of the application
+// The @SpringBootApplication annotation indicates that is a Spring Boot application
+// This is what starts when you run the application
+
 @SpringBootApplication
 public class FirstapiApplication {
-
+// The main method is the entry point of the application
 	public static void main(String[] args) {
 		SpringApplication.run(FirstapiApplication.class, args);
 	}
-
+// SpringApplication.run() method is used to launch the application
+	// It takes the application class and command line arguments as parameters
 }
--- Unit-9-Backend-REST-API-Entity-Framework/01-firstapi/src/main/java/com/firstapi/myController.java
+++ Unit-9-Backend-REST-API-Entity-Framework/01-firstapi/src/main/java/com/firstapi/myController.java
@@ -1,8 +1,14 @@
 package com.firstapi;
 
+// this file contains controllers to handle HTTP requests sent to this server.
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// The @RestController annotation indicates that this class has REST controllers to it
+// REST stands for Representational STate Transfer,
+//      WHich is an archatechtural style for designing networked applications.
+// REST is a type of API - Most p
 @RestController
 public class myController {
 
